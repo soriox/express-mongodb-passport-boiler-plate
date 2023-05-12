f9fa8e1cc0ea1f70fd5f3c1f63229e2347994bf9
Soriox <sorioxcode@gmail.com>
2023-05-12 19:21:40 -0400

    Fixed error handling function .. again :)

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const handle_error = require('../config/error_handler');
+const {handle_error} = require('../config/error_handler');
 
 const register = async (req, res) => {
     try {
