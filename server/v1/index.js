75af4ae75823843a69ff04b861b78d1113ccfc80
Soriox <sorioxcode@gmail.com>
2023-05-12 18:07:13 -0400

    Update index.js

--- server/v1/index.js
+++ server/v1/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 // API
 router.get('/', async (req, res) => {
     res.send({
-      name: 'Firefly API',
+      name: 'API',
       version: '1.0.0',
       description: 'This is a basic API that provides some sample endpoints',
-      website: 'https://www.fireflycms.com',
+      website: 'https://example.com',
     })
 });
 
